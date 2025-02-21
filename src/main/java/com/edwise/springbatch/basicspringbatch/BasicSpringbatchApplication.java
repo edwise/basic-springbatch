08b1fd9978c115bb87d9d005c9e75db936b234c9
edwise <eantonc@gmail.com>
2025-02-21 12:27:09 +0100

    Initial commit

---
+++ src/main/java/com/edwise/springbatch/basicspringbatch/BasicSpringbatchApplication.java
@@ -0,0 +1,13 @@
+package com.edwise.springbatch.basicspringbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicSpringbatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicSpringbatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edwise/springbatch/basicspringbatch/BasicSpringbatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edwise.springbatch.basicspringbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicSpringbatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
